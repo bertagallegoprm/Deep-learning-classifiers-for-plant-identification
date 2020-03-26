c48a5af41c52b0f57a8b97fed1c5c1361ca4f330
Berta Gallego <bgallegop@uoc.edu>
2020-03-26 22:10:21 +0000

    explore api request results

--- data/data-request.py
+++ data/data-request.py
@@ -6,12 +6,15 @@ def get_data():
     Get data from GBIF using its API
     """
     base_url = "https://api.gbif.org/v1/"
-    filter = {"mediaType": "StillImage", "country": "GB", "hasCoordinate": "True", "institutionCode": "K", "kingdom": "Plantae"}
+    # "institutionCode": "K"
+    filter = {"mediaType": "StillImage", "country": "GB", "hasCoordinate": "True", "kingdom": "Plantae", "basisOfRecord": "PRESERVED_SPECIMEN", "taxonKey": "328"}
     response = requests.get(f"{base_url}occurrence/search", params=filter)
     if response.status_code == 200:
         #print(response.headers["Date"])
         occurrences = response.json()
-        print(occurrences)
+        print(f"Keys: {occurrences.keys()}")
+        print(f"Results: {occurrences['results']}")
+        print(f"Number of occurrences: {occurrences['count']}")
     elif response.status_code == 404:
         print('404: Page not found.')
     else:
