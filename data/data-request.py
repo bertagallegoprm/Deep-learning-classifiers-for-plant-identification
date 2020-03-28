d0f9bd99faebdd6f3080c0b260c08752db8afdce
Berta Gallego <bgallegop@uoc.edu>
2020-03-28 22:54:25 +0000

    get taxon key for a species name using gbif api
    The taxon key is needed to filter the occurrences of the species selected.

--- data/data-request.py
+++ data/data-request.py
@@ -1,23 +1,27 @@
 import requests
 
 
-def get_taxon_key():
+def get_taxon_key(species_list):
     """
     Get the GBIF taxon key for a given species name
     using its API.
+    Return a dictionary for the species in the list passed as argument. 
     """
     base_url = "https://api.gbif.org/v1/"
-    filter = {"name": "Quercus robur"}
-    response = requests.get(f"{base_url}species", params=filter)
-    if response.status_code == 200:
-        #print(response.headers["Date"])
-        occurrences = response.json()
-        print(f"Keys: {occurrences.keys()}")
-        print(f"Results: {occurrences['results']}")
-    elif response.status_code == 404:
-        print('404: Page not found.')
-    else:
-        print(":(")
+    resource = "species/match"
+    parameter = "name"
+    species_dict = {}
+    for species_name in species_list:
+        response = requests.get(f"{base_url}{resource}?{parameter}={species_name}")
+        if response.status_code == 200:
+            species = response.json()
+            species_key = species["speciesKey"]
+            species_dict[species_name] = species_key
+        elif response.status_code == 404:
+            print('Error 404: Page not found.')
+        else:
+            print("Error. Undetermined status code.")
+    return species_dict
 
 
 def get_data():
@@ -40,5 +44,6 @@ def get_data():
         print(":(")
 
 if __name__ == "__main__":
-    get_taxon_key()
+    species_list = ("quercus robur", "taxus baccata")
+    print(get_taxon_key(species_list))
     #get_data()
