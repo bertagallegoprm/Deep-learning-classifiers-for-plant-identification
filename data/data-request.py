165d1b4ed3049b3856411e567f6b001f8316dbbf
Berta Gallego <bgallegop@uoc.edu>
2020-03-30 11:22:24 +0100

    get uk native tree names (dictionary)
    Source: https://www.woodlandtrust.org.uk/trees-woods-and-wildlife/british-trees/native-trees/
    Keys of the dictionary are the binomial names.
    Values are the vernaular names in UK. Vernacular names are  not needed for the data request in GBIF, but added now because it was not too much extra work and may be useful later.

--- data/data-request.py
+++ data/data-request.py
@@ -3,6 +3,63 @@ import urllib.request
 import os
 
 
+def native_trees_dict():
+    """
+    Return dictionary of native trees in Britain
+    (key: binomial name; value: vernacular name in UK).
+    Source: https://www.woodlandtrust.org.uk/trees-woods-and-wildlife/british-trees/native-trees/
+    """
+    dict = {
+        "Alnus glutinosa": "alder",
+        "Frangula alnus": "alder buckthorn",
+        "Fraxinus excelsior": "ash",
+        "Populus tremula": "aspen",
+        "Fagus sylbatica": "common beech",
+        "Betula pubescens": "downy birch",
+        "Betula pendula": "silver birch", 
+        "Prunus spinosa": "blackthorn",
+        "Buxus sempervirens": "common box",
+        "Rhamnus cathartica": "purging buckthorn",
+        "Prunus padus": "bird cherry",
+        "Prunus avium": "wild cherry",
+        "Malus sylvestris": "crab apple",
+        "Cornus sanguinea": "dogwood",
+        "Sambucus nigra": "elder",
+        "Ulmus procera": "English elm",
+        "Ulmus glabra": "wych elm",
+        "Viburnum opulus": "guelder rose",
+        "Crataegus monogyna": "hawthorn",
+        "Crataegus laevigata": "Midland hawthorn",
+        "Corylus avellana": "hazel",
+        "Ilex aquifolium": "holly",
+        "Carpinus betulus": "hornbeam",
+        "Juniperus communis": "juniper",
+        "Tilia x europaea": "common lime", 
+        "Tilia platyphyllos": "large-leaved lime",
+        "Tilia cordata": "small-leaved lime",
+        "Acer campestre": "field maple",
+        "Quercus robur": "English oak",
+        "Quercus petraea": "sessile oak",
+        "Pyrus cordata": "Plymouth pear",
+        "Pinus sylvestris": "Scots pine", 
+        "Populus nigra": "black poplar", 
+        "Sorbus aucuparia": "rowan", 
+        "Euonymus europaea": "spindle",
+        "Sorbus aria": "whitebeam", 
+        "Sorbus arranensis": "Arran whitebeam",
+        "Sorbus rupicola": "rock whitebeam",
+        "Sorbus torminalis": "wild service tree",
+        "Salix pentandra": "bay willow",
+        "Salix fragilis": "crack willow",
+        "Salix caprea": "goat willow",
+        "Salix cinerea subsp. oleifolia": "grey willow",
+        "Salix viminalis": "osier willow",
+        "Salix alba": "white willow",
+        "Taxus baccata": "yew"
+    }
+    return dict
+
+
 def get_taxon_key(species_list):
     """
     Get the GBIF taxon key for a given species name
