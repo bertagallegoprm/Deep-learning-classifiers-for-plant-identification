6e882a5ed61980d9c5b8aad767fb27a6baa528ad
Berta Gallego <bgallegop@uoc.edu>
2020-04-26 00:32:24 +0100

    outline script functionality

---
+++ data/request_stats.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+if __name__ == "__main__":
+
+    # Open request_results into a data frame
+
+    # Table with Species name : Occurrences/species : Images/species
+    ## List of species
+    ## Number of occurences by species
+    ## Number of images per species
+
+
+    # Total number of different species
+    # Total number of occurrences
+    # Total number of images
