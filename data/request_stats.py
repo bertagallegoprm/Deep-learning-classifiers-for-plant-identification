c900ca572e9beb78f74c314e547cf8ff8480eb78
Berta Gallego <bgallegop@uoc.edu>
2020-04-26 15:41:24 +0100

    read csv with the request results and store it as pandas data frame

--- data/request_stats.py
+++ data/request_stats.py
@@ -1,8 +1,11 @@
 import pandas as pd
+from file_handler import read_csv_name, create_dataframe_from_csv
+
 
 if __name__ == "__main__":
 
     # Open request_results into a data frame
+    request_results = create_dataframe_from_csv(read_csv_name())
 
     # Table with Species name : Occurrences/species : Images/species
     ## List of species
