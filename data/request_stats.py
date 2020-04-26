5ebb6d43a491b32c7f451dfd0367d1c9fed4ce4e
Berta Gallego <bgallegop@uoc.edu>
2020-04-26 16:58:09 +0100

    get number of occurrences per species

--- data/request_stats.py
+++ data/request_stats.py
@@ -5,13 +5,15 @@ from file_handler import read_csv_name, create_dataframe_from_csv
 if __name__ == "__main__":
 
     # Open request_results into a data frame
-    request_results = create_dataframe_from_csv(read_csv_name())
+    #request_results = create_dataframe_from_csv(read_csv_name())
+    request_results = create_dataframe_from_csv("/home/berta/Repos/tfm/data/request_reports/ef1a27f6cfc1b4755c1c380f9a54ee9c_request_results.csv")
 
     # Table with Species name : Occurrences/species : Images/species
     ## List of species
     unique_taxon = request_results[["species_name", "taxon_key"]].drop_duplicates(["species_name", "taxon_key"])
     print(unique_taxon)
     ## Number of occurences by species
+    occurrences_per_taxon = request_results.groupby("species_name")["taxon_key"].value_counts().reset_index(name="occurrence_count")
     ## Number of images per species
 
 
