e71cfeae8e69aa1958dd1ac2340be0e90c48e6af
Berta Gallego <bgallegop@uoc.edu>
2020-04-26 17:30:12 +0100

    group get summary as a function

--- data/request_stats.py
+++ data/request_stats.py
@@ -1,27 +1,26 @@
 import pandas as pd
 from file_handler import read_csv_name, create_dataframe_from_csv
 
+def get_summary_of_results(path_to_file):
+    """
+    Given the path to the CSV file with the results of the request:
+    Return a data frame with a summary of
+    - Number of occurrences species
+    - Number of images per species
+    """
+    request_results = create_dataframe_from_csv(path_to_file)
+    occurrences_per_taxon = request_results.groupby("species_name")["taxon_key"].value_counts().reset_index(name="occurrence_count")
+    images_per_taxon = request_results[request_results.has_image == 1].groupby("species_name")["taxon_key"].value_counts().reset_index(name="image_count")
+    images_per_taxon = images_per_taxon.iloc[:,-1]
+    return occurrences_per_taxon.join(images_per_taxon)
 
 if __name__ == "__main__":
 
     # Open request_results into a data frame
-    #request_results = create_dataframe_from_csv(read_csv_name())
-    request_results = create_dataframe_from_csv("/home/berta/Repos/tfm/data/request_reports/ef1a27f6cfc1b4755c1c380f9a54ee9c_request_results.csv")
+    path_to_file = read_csv_name()
+    summary = get_summary_of_results(path_to_file)
 
-    # Table with Species name : Occurrences/species : Images/species
-    ## List of species
-    unique_taxon = request_results[["species_name", "taxon_key"]].drop_duplicates(["species_name", "taxon_key"])
-    print(unique_taxon)
-    ## Number of occurences by species
-    occurrences_per_taxon = request_results.groupby("species_name")["taxon_key"].value_counts().reset_index(name="occurrence_count")
-    ## Number of images per species
-    images_per_taxon = request_results[request_results.has_image == 1].groupby("species_name")["taxon_key"].value_counts().reset_index(name="image_count")
-    images_per_taxon = images_per_taxon.iloc[:,-1]
-
-    # Join counts
-    summary = occurrences_per_taxon.join(images_per_taxon)
-    #print(summary)
-
+    summary = get_summary_of_results(request_results)
     # Total number of different species
     total_species = int(summary.shape[0])
     print(f"Total number of species: {total_species}")
