65649fbf85c69eca4e96c5dabc4e554165d16629
Berta Gallego <bgallegop@uoc.edu>
2020-04-26 17:58:32 +0100

    save summary of results as a CSV

--- data/request_stats.py
+++ data/request_stats.py
@@ -14,11 +14,22 @@ def get_summary_of_results(path_to_file):
     images_per_taxon = images_per_taxon.iloc[:,-1]
     return occurrences_per_taxon.join(images_per_taxon)
 
+def save_summary_as_csv(path_to_file):
+    """
+    Given the path to the CSV file with the results of the request
+    creates a summary as a data frame object using get_summary_of_results()
+    and saves it to the request_reports folders as a CSV.
+    """
+    summary = get_summary_of_results(path_to_file)
+    file_name = path_to_file.split("/")[-1]
+    filter_hash = file_name.split("_")[0]
+    summary_file_name = "request_reports/" + filter_hash + "_summary.csv"
+    summary.to_csv(summary_file_name)
+    print(f"File {summary_file_name} created.")
+
 if __name__ == "__main__":
 
-    # Open request_results into a data frame
-    path_to_file = read_csv_name()
-    summary = get_summary_of_results(path_to_file)
+    save_summary_as_csv(read_csv_name())
 
     summary = get_summary_of_results(request_results)
     # Total number of different species
