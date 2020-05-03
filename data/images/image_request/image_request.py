ced6715be250e27aabd66a0922d8983dc9ab7872
Berta Gallego <bgallegop@uoc.edu>
2020-05-03 23:24:15 +0100

    modify image request structure

--- data/images/image_request/image_request.py
+++ data/images/image_request/image_request.py
@@ -4,60 +4,10 @@ import os
 import pandas as pd
 import hashlib
 import pdb
-from species_names import native_trees_list
-from storage_handler import stop_if_size
-from file_handler import request_result_to_csv, open_filter_report
-
-def get_taxon_key(species_list):
-    """
-    Get the GBIF taxon key for a given species name
-    using its API.
-    Return a dictionary for the species in the list passed as argument. 
-    """
-    base_url = "https://api.gbif.org/v1/"
-    resource = "species/match"
-    parameter = "name"
-    species_dict = {}
-    for species_name in species_list:
-        response = requests.get(f"{base_url}{resource}?{parameter}={species_name}")
-        if response.status_code == 200:
-            try:
-                species = response.json()
-                species_key = species["speciesKey"]
-                species_dict[species_name] = species_key
-            except:
-                #print(f"Unable to find speciesKey for {species_name}.")
-                pass
-        elif response.status_code == 404:
-            print('Error 404: Page not found.')
-        else:
-            print("Error. Undetermined status code.")
-    return species_dict
-
-
-def get_occurence_key(species_taxon_key, filter):
-    """
-    Get data from GBIF using its API
-    """
-    base_url = "https://api.gbif.org/v1/"
-    occurences_dict = {}
-    for species_name, taxon_key in species_taxon_key.items():
-        filter["taxonKey"]=taxon_key
-        response = requests.get(f"{base_url}occurrence/search", params=filter)
-        if response.status_code == 200:
-            occurrences = response.json()
-            occurrences_results = occurrences.get("results",{})
-            species_occurence_dict = {}
-            for occurrence_no in range(0, len(occurrences_results)):
-                occurences_key = occurrences_results[occurrence_no].get("key")
-                species_occurence_dict[occurrence_no] = occurences_key        
-        elif response.status_code == 404:
-            print('Error 404: Page not found.')
-        else:
-            print("Error. Undetermined status code.")
-        occurences_dict[species_name]=species_occurence_dict
-        #print(f"Number of occurrences for {species_name}: {occurrences['count']}")
-    return occurences_dict
+from data.species_names import native_trees_list
+from data.storage_handler import stop_if_size
+from data.file_handler import request_result_to_csv, open_filter_report
+from data.common_api_request import get_taxon_key, get_occurence_key
 
 
 def get_occurrence_image(species_occurrences_keys, folder):
