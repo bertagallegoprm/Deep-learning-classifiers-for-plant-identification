16405664d6b9368c875f5819ebd81d2a09d0d33c
Berta Gallego <bgallegop@uoc.edu>
2020-05-11 23:10:16 +0100

    refactor crop_image

--- data/images/image_preprocessing/image_crop.py
+++ data/images/image_preprocessing/image_crop.py
@@ -4,6 +4,25 @@ import errno
 import shutil
 
 
+def scale_image(image):
+    """
+    Given an image stored as tensor,
+    scale to a fixed pixel number.
+    (From documentation: "Resizes an image to a target width and height
+    by keeping the aspect ratio the same without distortion. 
+    If the target dimensions don't match the image dimensions, 
+    the image is resized and then padded with zeroes 
+    to match requested dimensions.)
+    """
+    target_height = 150
+    target_width = 150
+    tf.image.resize_with_pad(
+    image, target_height, target_width, method=ResizeMethod.BILINEAR,
+    antialias=False
+    )
+    return image
+
+
 def create_empty_dir(base_path, directory):
     """
     Create a new directory.
@@ -22,7 +41,18 @@ def create_empty_dir(base_path, directory):
         return full_path
 
 
-def crop_image(source_dir, destination_dir, image_file):
+def jpg_to_tensor(source_dir, image_file):
+    """
+    Given a path to an image,
+    return a 3-D tensor.
+    """
+    try:
+        return tf.image.decode_jpeg(tf.io.read_file(f"{source_dir}/{image_file}"), channels=3)
+    except:
+        return print(f"Unable to open image {image_file}.")
+
+
+def crop_image(tensor, image_file):
     """
     Crop image at a fixed position.
     Documentation: https://www.tensorflow.org/api_docs/python/tf/image/crop_to_bounding_box
@@ -34,33 +64,29 @@ def crop_image(source_dir, destination_dir, image_file):
     # Target variables values 
     target_height = 100 # Height of the result
     target_width = 100 # Width of the result
-
-    # Import the image
     try:
-        image = tf.image.decode_jpeg(tf.io.read_file(f"{source_dir}/{image_file}"), channels=3)
-        # Scale
-        
-        # Crop the image as per the parameters
-        try:
-            cropped_image_tensor = tf.image.crop_to_bounding_box(image, offset_height, offset_width, target_height, target_width)
-            output_image = tf.image.encode_jpeg(cropped_image_tensor)
-            # Save image
-            try:
-                # Create species folder if it does not exist
-                if not os.path.exists(destination_dir):
-                    os.mkdir(destination_dir)
-                # Create a constant as filename
-                image_crop_path = os.path.join(destination_dir, image_file)
-                file_name = tf.constant(image_crop_path)
-                tf.io.write_file(file_name, output_image)
-            except:
-                return print(f"Unable to save image {image_file}.")
-            else:
-                print(f"{image_file} saved to {image_crop_path}.")
-        except:
-            return print(f"Unable to crop image {image_file}.")
+        return tf.image.crop_to_bounding_box(tensor, offset_height, offset_width, target_height, target_width)
     except:
-        return print(f"Unable to open image {image_file}.")
+        return print(f"Unable to crop image {image_file}.")
+
+
+def tensor_to_jpg(tensor, destination_dir, image_file):
+    """
+    Given a 3-D tensor
+    return a .jpg image and save it to destination.
+    """
+    try:
+        output_image = tf.image.encode_jpeg(tensor)
+        if not os.path.exists(destination_dir):
+            os.mkdir(destination_dir)
+        # Create a constant as filename
+        image_path = os.path.join(destination_dir, image_file)
+        file_name = tf.constant(image_path)
+        tf.io.write_file(file_name, output_image)
+    except:
+        print(f"Unable to save image {image_file}.")
+    else:
+        print(f"{image_file} saved to {image_path}.")
 
 
 def crop_all_raw_images(source_dir, destination_dir):
@@ -71,10 +97,9 @@ def crop_all_raw_images(source_dir, destination_dir):
     if os.path.exists(source_dir):
         for species_folder in os.listdir(source_dir):
             for image_file in os.listdir(os.path.join(source_dir, species_folder)):
-                try:
-                    crop_image(os.path.join(source_dir, species_folder), os.path.join(destination_dir, species_folder), image_file)
-                except:
-                    pass
+                tensor = jpg_to_tensor(os.path.join(source_dir, species_folder), image_file)
+                crop_image(tensor, image_file)
+                tensor_to_jpg(tensor, os.path.join(destination_dir, species_folder), image_file)
     else:
         print(f"Folder {source_dir} not found.")
     print("End.")
