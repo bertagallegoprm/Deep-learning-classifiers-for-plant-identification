ea43d9aea55c798060ef528e63c63f4a6613fe74
Berta Gallego <bgallegop@uoc.edu>
2020-05-12 20:16:25 +0100

    create dataframe only with coordinates

--- data/geodata/preprocessing/plot_coordinates.py
+++ data/geodata/preprocessing/plot_coordinates.py
@@ -1,6 +1,8 @@
+import pandas as pd
 from data.search import filter_hash
 from data.config import geodata_filter, species_list
-from data.file_handler import create_dataframe_from_csv
+from data.file_handler import create_dataframe_from_csv, column_to_list
+
 
 if __name__ == "__main__":
 
@@ -9,4 +11,11 @@ if __name__ == "__main__":
     filter_hash = filter_hash(geodata_filter, species_list.species_list)
     csv_file_name = filter_hash + "_geodata.csv"
     df = create_dataframe_from_csv(base_path+csv_file_name)
-    print(df)
+
+    # Extract list with coordinates
+    latitude = column_to_list(df, "decimal_latitude")
+    longitude = column_to_list(df, "decimal_longitude")
+    species_name = column_to_list(df, "species_name")
+    df_map = pd.DataFrame(list(zip(species_name, latitude, longitude)), 
+    columns = ["species_name", "latitude", "longitude"])
+    print(df_map)
