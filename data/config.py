17ba85c717633c4166f8d41b535a5b7a35f3f29a
Berta Gallego <bgallegop@uoc.edu>
2020-06-12 00:31:55 +0100

    delete ImageDataGenerator in config, as the parameters are set in the model notebooks

--- data/config.py
+++ data/config.py
@@ -47,22 +47,3 @@ resizing = ImageDimension(
     width = 1200,
     height = 2000
 )
-
-
-class ImageGenerator:
-    """
-    color_mode - "rgb" for 3 color channel, "grayscale" for black and white.
-    class_mode - "binary" for 2 classes, "categoriacal" for more than 2, "input" for autoencoder.
-    batch sixe - No. of images to be yielded from the generator per batch.
-    epoch - 
-    shuffle - True to shuffle the order of images being yielded. False otherwise.
-    seed - seed for the random image augmentation
-    """
-    color_mode= "rgb"   
-    img_height = 150 # if 50: pixels too big; if 250: too much noise
-    img_width = 150   
-    class_mode="categorical" 
-    batch_size = 3 
-    epochs = 15                                     
-    shuffle=True                                                               
-    seed = 1234 
